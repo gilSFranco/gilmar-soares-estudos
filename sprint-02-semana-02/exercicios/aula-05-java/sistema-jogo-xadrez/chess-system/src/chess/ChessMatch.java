a8b8dec083ea7c125f938f614b4a4107b6b4fa40
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-08-16 21:04:03 -0300

    Handling captured pieces

--- sprint-02-semana-02/exercicios/aula-05-java/sistema-jogo-xadrez/chess-system/src/chess/ChessMatch.java
+++ sprint-02-semana-02/exercicios/aula-05-java/sistema-jogo-xadrez/chess-system/src/chess/ChessMatch.java
@@ -6,15 +6,23 @@ import boardgame.Piece;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private Board board;
 
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
+
     public ChessMatch() {
         board = new Board(8,8);
         turn = 1;
         currentPlayer = Color.WHITE;
+
+
         initialSetup();
     }
 
@@ -63,6 +71,11 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
 
+        if(capturedPiece != null) {
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
+
         board.placePiece(p, target);
 
         return capturedPiece;
@@ -93,6 +106,8 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup() {
