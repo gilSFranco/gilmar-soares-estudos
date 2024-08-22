a1d29d2cb0751f1be90a1dcffe264400952a554f
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-08-22 10:53:56 -0300

    findById implementation

--- sprint-03-semana-03/exercicios-do-curso/banco-de-dados-jdbc/demo-dao-jdbc/demo/src/application/Main.java
+++ sprint-03-semana-03/exercicios-do-curso/banco-de-dados-jdbc/demo-dao-jdbc/demo/src/application/Main.java
@@ -9,12 +9,9 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department department = new Department(1, "Books");
-        System.out.println(department);
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
