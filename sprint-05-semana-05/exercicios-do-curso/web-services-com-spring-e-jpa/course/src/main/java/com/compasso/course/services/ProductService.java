6888d35e4bd996da7dffb078d065adcdfb3201e1
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-03 13:17:41 -0300

    Product

---
+++ sprint-05-semana-05/exercicios-do-curso/web-services-com-spring-e-jpa/course/src/main/java/com/compasso/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.compasso.course.services;
+
+import com.compasso.course.entities.Product;
+import com.compasso.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> product = repository.findById(id);
+        return product.get();
+    }
+}
