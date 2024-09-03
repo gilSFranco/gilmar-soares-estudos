60290d030cb8d357fbb064df48a733835263562c
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-03 12:35:32 -0300

    Category

--- sprint-05-semana-05/exercicios-do-curso/web-services-com-spring-e-jpa/course/src/main/java/com/compasso/course/config/TestConfig.java
+++ sprint-05-semana-05/exercicios-do-curso/web-services-com-spring-e-jpa/course/src/main/java/com/compasso/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.compasso.course.config;
 
+import com.compasso.course.entities.Category;
 import com.compasso.course.entities.Order;
 import com.compasso.course.entities.User;
 import com.compasso.course.entities.enums.OrderStatus;
+import com.compasso.course.repositories.CategoryRepository;
 import com.compasso.course.repositories.OrderRepository;
 import com.compasso.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
