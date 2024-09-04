d969eaceda84f08bb3d1d236a50ddad00dec5d12
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-04 11:40:28 -0300

    Exception handling - findById

--- sprint-05-semana-05/exercicios-do-curso/web-services-com-spring-e-jpa/course/src/main/java/com/compasso/course/services/UserService.java
+++ sprint-05-semana-05/exercicios-do-curso/web-services-com-spring-e-jpa/course/src/main/java/com/compasso/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.compasso.course.services;
 
 import com.compasso.course.entities.User;
 import com.compasso.course.repositories.UserRepository;
+import com.compasso.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
