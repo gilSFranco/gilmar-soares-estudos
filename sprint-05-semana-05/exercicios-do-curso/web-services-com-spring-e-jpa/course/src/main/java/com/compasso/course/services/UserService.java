fff5bf2651931e3c9e2d56ee184eff22a2e98500
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-04 12:28:43 -0300

    Exception handling - update

--- sprint-05-semana-05/exercicios-do-curso/web-services-com-spring-e-jpa/course/src/main/java/com/compasso/course/services/UserService.java
+++ sprint-05-semana-05/exercicios-do-curso/web-services-com-spring-e-jpa/course/src/main/java/com/compasso/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.compasso.course.entities.User;
 import com.compasso.course.repositories.UserRepository;
 import com.compasso.course.services.exceptions.DatabaseException;
 import com.compasso.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
