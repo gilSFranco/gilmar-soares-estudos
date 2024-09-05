0a494ef75da637369565e1568ea902145e56811b
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-05 13:35:42 -0300

    User insertion with POST

--- sprint-05-semana-05/exercicios-do-curso/api-restful-spring-boot-mongodb/workshopmongo/src/main/java/com/compasso/workshopmongo/resources/UserResource.java
+++ sprint-05-semana-05/exercicios-do-curso/api-restful-spring-boot-mongodb/workshopmongo/src/main/java/com/compasso/workshopmongo/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.compasso.workshopmongo.dto.UserDTO;
 import com.compasso.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,13 +25,20 @@ public class UserResource {
         List<UserDTO> listDto = list.stream()
                 .map(x -> new UserDTO(x))
                 .collect(Collectors.toList());
-
         return ResponseEntity.ok().body(listDto);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-        User list = service.findById(id);
-        return ResponseEntity.ok().body(new UserDTO(list));
+        User object = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(object));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objectDto) {
+        User object = service.fromDTO(objectDto);
+        object = service.insert(object);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(object.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- sprint-05-semana-05/exercicios-do-curso/api-restful-spring-boot-mongodb/workshopmongo/src/main/java/com/compasso/workshopmongo/services/UserService.java
+++ sprint-05-semana-05/exercicios-do-curso/api-restful-spring-boot-mongodb/workshopmongo/src/main/java/com/compasso/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.compasso.workshopmongo.services;
 
 import com.compasso.workshopmongo.domain.User;
+import com.compasso.workshopmongo.dto.UserDTO;
 import com.compasso.workshopmongo.repository.UserRepository;
 import com.compasso.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> object = repository.findById(id);
         return object.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User object) {
+        return repository.save(object);
+    }
+
+    public User fromDTO(UserDTO object) {
+        return new User(object.getId(), object.getName(), object.getEmail());
     }
 }
