ca7c28710126992f6922326ac785006bdd8448cf
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-05 15:13:53 -0300

    Projection of Author data with DTO

---
+++ sprint-05-semana-05/exercicios-do-curso/api-restful-spring-boot-mongodb/workshopmongo/src/main/java/com/compasso/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,34 @@
+package com.compasso.workshopmongo.dto;
+
+import com.compasso.workshopmongo.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+    private String id;
+    private String name;
+
+    public AuthorDTO() {
+    }
+
+    public AuthorDTO(User object) {
+        id = object.getId();
+        name = object.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
