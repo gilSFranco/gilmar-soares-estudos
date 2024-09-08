1f27432de878ff50782562405f8387c0993e4f04
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-08 15:42:27 -0300

    feat: project created

---
+++ sprint-05-semana-05/atividade-05/atividadesemanal/src/main/java/com/compasso/atividadesemanal/AtividadesemanalApplication.java
@@ -0,0 +1,13 @@
+package com.compasso.atividadesemanal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadesemanalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadesemanalApplication.class, args);
+	}
+
+}
---
+++ sprint-05-semana-05/atividade-05/atividadesemanal/src/test/java/com/compasso/atividadesemanal/AtividadesemanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compasso.atividadesemanal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadesemanalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
