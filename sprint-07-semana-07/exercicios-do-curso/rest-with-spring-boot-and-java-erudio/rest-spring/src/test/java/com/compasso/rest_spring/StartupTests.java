487729852da55cc9c68a0630d2e7892d251579b1
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-19 12:52:52 -0300

    feat: project rest with spring boot created

---
+++ sprint-07-semana-07/exercicios-do-curso/rest-with-spring-boot-and-java-erudio/rest-spring/src/test/java/com/compasso/rest_spring/StartupTests.java
@@ -0,0 +1,13 @@
+package com.compasso.rest_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
