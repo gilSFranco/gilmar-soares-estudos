72dcde0be0b789375f9069e7932748c7fd53d0b7
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-20 16:04:08 -0300

    feat: week 07 activity - project created

---
+++ sprint-07-semana-07/atividade-07/api-rest-name/src/main/java/com/compasso/api_rest_name/ApiRestNameApplication.java
@@ -0,0 +1,13 @@
+package com.compasso.api_rest_name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestNameApplication.class, args);
+	}
+
+}
---
+++ sprint-07-semana-07/atividade-07/api-rest-name/src/test/java/com/compasso/api_rest_name/ApiRestNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compasso.api_rest_name;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
