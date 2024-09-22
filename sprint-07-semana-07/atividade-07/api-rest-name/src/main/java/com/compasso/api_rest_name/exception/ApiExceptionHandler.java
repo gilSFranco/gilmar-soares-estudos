3b06d381b50e780b2a7da7caf32e92bc3f411304
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-22 15:18:33 -0300

    feat: handling validation exceptions

--- sprint-07-semana-07/atividade-07/api-rest-name/src/main/java/com/compasso/api_rest_name/exception/ApiExceptionHandler.java
+++ sprint-07-semana-07/atividade-07/api-rest-name/src/main/java/com/compasso/api_rest_name/exception/ApiExceptionHandler.java
@@ -1,4 +1,47 @@
 package com.compasso.api_rest_name.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
 public class ApiExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request, BindingResult bindingResult) {
+        log.error("Api Error - ", e);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(
+                        request,
+                        HttpStatus.UNPROCESSABLE_ENTITY,
+                        "A validação detectou um erro. Causa(s):",
+                        bindingResult
+                ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> internalServerErrorException(Exception e, HttpServletRequest request) {
+        ErrorMessage error =
+                new ErrorMessage(
+                        request,
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()
+                );
+
+        log.error("Internal Server Error {} | {} ", error, e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(error);
+    }
 }
