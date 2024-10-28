de6a13e3f43d13c84c5d1c3be643d7426daf3521
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-10-28 17:13:52 -0300

    feat: service layer - new user registration method created

--- sprint-12-semana-12/atividade-12/aws-springboot/src/main/java/com/compass/aws_springboot/service/UserService.java
+++ sprint-12-semana-12/atividade-12/aws-springboot/src/main/java/com/compass/aws_springboot/service/UserService.java
@@ -1,11 +1,29 @@
 package com.compass.aws_springboot.service;
 
+import com.compass.aws_springboot.entities.User;
 import com.compass.aws_springboot.repository.UserRepository;
+import com.compass.aws_springboot.web.dto.UserDTO;
+import com.compass.aws_springboot.web.dto.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional
+    public User createUser(UserDTO userDTO) {
+        User newUser = UserMapper.toUser(userDTO);
+
+        newUser.setZipCode("12345678");
+        newUser.setStreet("Example Street");
+        newUser.setComplement("Apt 01");
+        newUser.setNeighborhood("Example Neighborhood");
+        newUser.setCity("Example City");
+        newUser.setState("SP");
+
+        return userRepository.save(newUser);
+    }
 }
