0928940a62c35687eb1e60eaf0e387768fdde573
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-10-28 16:37:15 -0300

    feat: web layer - UserDTO and ResponseUserDTO classes created (the pom was updated to accommodate the changes made)

--- sprint-12-semana-12/atividade-12/aws-springboot/src/main/java/com/compass/aws_springboot/web/dto/UserDTO.java
+++ sprint-12-semana-12/atividade-12/aws-springboot/src/main/java/com/compass/aws_springboot/web/dto/UserDTO.java
@@ -1,4 +1,24 @@
 package com.compass.aws_springboot.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserDTO {
+
+    @NotBlank(message = "Username cannot be blank or null")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank or null")
+    private String password;
+
+    @NotBlank(message = "Email cannot be blank or null")
+    private String email;
+
+    @NotBlank(message = "Cep cannot be blank or null")
+    private String cep;
 }
